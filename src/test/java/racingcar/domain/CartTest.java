39357bddd7d56da258a4cdde2ef63c3b4a3e1ea4
Beomsu <gobeumsu@gmail.com>
2023-11-01 14:11:19 +0900

    test : 입력으로 들어온 위치와 현재 자동차 위치 비교

---
+++ src/test/java/racingcar/domain/CartTest.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CartTest {
+
+    private Car testCar;
+
+    @BeforeEach
+    void setUp() {
+        testCar = Car.create("pobi");
+    }
+
+    @Test
+    @DisplayName("입력으로 들어온 위치와 현재 자동차의 위치가 같다면 true를 반환한다.")
+    void givenSamePosition_thenReturnTrue() {
+        // given
+        Car diffCar = Car.create("crong");
+        // when
+        boolean isSamePosition = testCar.isSamePosition(diffCar);
+        // then
+        assertThat(isSamePosition).isTrue();
+    }
+
+}
